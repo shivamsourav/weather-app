07577855055c1ebd83d4a4c32182fc201894e577
SHIVAM SOURAV <shivamsourav2001@gmail.com>
2023-04-24 11:06:40 +0530

    Footer added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Weather from "./components/weather";
 import "./components/index.css";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
       <Weather />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      Copyrights &copy;
+      <a
+        href="https://github.com/shivamsourav"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Shivam Sourav
+      </a>
+    </div>
+  );
+}
